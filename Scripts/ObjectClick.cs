04f8abd04c531b6430b8affcddc3b6f01a660a8b
lilsunshine <42014182+lilsunshine@users.noreply.github.com>
2018-08-25 00:45:33 -0400

    Add files via upload

---
+++ Scripts/ObjectClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        Debug.Log("These ruins look otherworldly... Could they be related to the spaceship landing?");
+    }
+
+}
+
---
+++ Scripts/ObjectClickSun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickSun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        Debug.Log("One of the two suns on this planet. They always mimic each others movements, as though they're in a mirror.");
+    }
+
+}
+
