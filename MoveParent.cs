a047c403a01b57559a9efb0d46e497f8a1e88d07
lilsunshine <42014182+lilsunshine@users.noreply.github.com>
2018-08-25 20:17:34 -0400

    Add files via upload

---
+++ MoveParent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveParent : MonoBehaviour {
+    public float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0);
+        transform.position += move * 3 * Time.deltaTime;
+    }
+}
---
+++ WrapObjects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("WillWrap"))
+        {
+            col.gameObject.transform.position = col.gameObject.transform.position + new Vector3(-26.675f, 0.0f, 0.0f);
+            Debug.Log("wrap");
+        }
+    }
+}
